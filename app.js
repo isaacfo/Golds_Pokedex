752827f41a97fdd0e5ae559950ba6bee69bc2a20
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-02-18 17:07:38 -0500

    added css and html for card generation

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const pokedex = document.getElementById('pokedex');
+console.log(pokedex);
+
 const fetchPokemon = () => {
   const promises = [];
-  for (let i = 1; i <= 150; i++) {
+  for (let i = 152; i <= 251; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     promises.push(fetch(url).then(res => res.json()));
   }
@@ -18,6 +21,18 @@ const fetchPokemon = () => {
 
 const displayPokemon = pokemon => {
   console.log(pokemon);
+  const pokemonHTMLString = pokemon
+    .map(
+      pokeman => `
+  <li class= "card">
+  <img class= "card-image" src = "${pokeman.image}"/>
+  <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+  <p class="card-subtitle">Type: ${pokeman.type}</p>
+  </li>`
+    )
+    // makes it all one string because map returned an array
+    .join('');
+  pokedex.innerHTML = pokemonHTMLString;
 };
 
 fetchPokemon();
