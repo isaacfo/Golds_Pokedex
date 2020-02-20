577e26d98344e198a59b7dcd66d91d207c1759a8
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-02-20 13:43:22 -0500

    added stat bars

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const pokedex = document.getElementById('pokedex');
+// const searchBar = document.getElementById('search-types');
+
 const cachedPokemon = {};
 
 const fetchPokemon = async () => {
-  const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=251`;
   const res = await fetch(url);
   const data = await res.json();
   const pokemon = data.results.map((data, index) => ({
@@ -47,13 +49,33 @@ const displayPokemanPopup = pokeman => {
   console.log(pokeman);
   const type = pokeman.types.map(type => type.type.name).join(', ');
   const ability = pokeman.abilities.map(ability => ability.ability.name);
+  const statSpeed = pokeman.stats[0].base_stat;
+  const statSpDefense = pokeman.stats[1].base_stat;
+  const statSpAttack = pokeman.stats[2].base_stat;
+  const statDefense = pokeman.stats[3].base_stat;
+  const statAttack = pokeman.stats[4].base_stat;
+  const statHp = pokeman.stats[5].base_stat;
+
   const htmlString = `
         <div class="popup">
             <button id="closeBtn" onclick="closePopup()">Close</button>
             <div class="card">
                 <img class="card-image" src="${pokeman.sprites['front_shiny']}"/>
                 <h2 class="card-title">${pokeman.name}</h2>
-                <p><small>Type: ${type} | Height:</small> ${pokeman.height} | Weight: ${pokeman.weight} | Abilities: ${ability}</p>
+                <p>Type: ${type} | Height: ${pokeman.height} | Weight: ${pokeman.weight} | Abilities: ${ability}</p>
+                <div class= "stats">Base Stats:<br>
+                HP: <progress value=${statHp} max="200">45</progress> <br>
+                Attack: <progress value=${statAttack} max="200"> </progress> <br> 
+                Defense: <progress value=${statDefense} max="200"> </progress> <br>
+                Special-Attack: <progress value=${statSpAttack} max="200"></progress> <br>
+                Special-Defense: <progress value=${statSpDefense} max="200"></progress><br> 
+                Speed: <progress value=${statSpeed} max="200"></progress> <br>
+                 
+                
+                
+                
+                
+                </div> 
             </div>
         </div>
     `;
@@ -66,3 +88,15 @@ const closePopup = () => {
 };
 
 fetchPokemon();
+
+// searchBar.addEventListener('keyup', function(e) {
+//   const searchType = e.target.value.toLowerCase();
+//   const pokemonTypes = htmlString.getElementByClassName('card');
+
+//   Array.from(pokemonTypes).forEach(function(pokemonType) {
+//     const pokeType = type.lastElementChild.textContent;
+//     if (pokeType.toLowerCase().indexof(searchType) != -1) {
+//       pokemonTypes.style.display = 'block';
+//     }
+//   });
+// });
